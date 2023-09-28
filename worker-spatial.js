f20ef52402741abae67b62e1ccfac07dc99f62cf
BagasNS <mrbagasnanda@gmail.com>
2023-09-29 06:28:46 +0700

    fix: function name

--- worker-spatial.js
+++ worker-spatial.js
@@ -1,22 +1,22 @@
 const { parentPort, workerData } = require("worker_threads");
 const turf = require('@turf/turf')
 
-function tile2lat(y, z) {
+function _tile2lat(y, z) {
   var n = Math.PI - 2 * Math.PI * y / Math.pow(2, z);
   return (180 / Math.PI * Math.atan(0.5 * (Math.exp(n) - Math.exp(-n))));
 }
 
-function tile2long(x, z) {
+function _tile2long(x, z) {
   return (x / Math.pow(2, z) * 360 - 180);
 }
 
 function _getTileRect(x, y, zoom) {
-  const c1 = turf.point([tile2long(x, zoom), tile2lat(y, zoom)]);
-  const c2 = turf.point([tile2long(x + 1, zoom), tile2lat(y + 1, zoom)]);
+  const c1 = turf.point([_tile2long(x, zoom), _tile2lat(y, zoom)]);
+  const c2 = turf.point([_tile2long(x + 1, zoom), _tile2lat(y + 1, zoom)]);
   return turf.bboxPolygon(turf.bbox(turf.featureCollection([c1, c2])));
 }
 
-function generateGridTilesWorker(selectedRegion, zoomLevel, tileRange) {
+function _generateGridTilesWorker(selectedRegion, zoomLevel, tileRange) {
   const gridTiles = [];
 
   for (let y = tileRange.startY; y <= tileRange.endY; y++) {
@@ -34,6 +34,6 @@ function generateGridTilesWorker(selectedRegion, zoomLevel, tileRange) {
 
 parentPort?.once('message', () => {
   const { selectedRegion, zoomLevel, tileRange } = workerData;
-  const result = generateGridTilesWorker(selectedRegion, zoomLevel, tileRange);
+  const result = _generateGridTilesWorker(selectedRegion, zoomLevel, tileRange);
   parentPort?.postMessage(result);
 });
