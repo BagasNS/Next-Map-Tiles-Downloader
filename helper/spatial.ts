6742d52f38abd7bc5636a745021476e5bdcf546a
BagasNS <mrbagasnanda@gmail.com>
2023-09-29 06:28:00 +0700

    fix: remove unused code

--- helper/spatial.ts
+++ helper/spatial.ts
@@ -1,13 +1,10 @@
 // @ts-ignore
-import { Feature, featureCollection, point } from "@turf/helpers";
-// @ts-ignore
-import BBOX from '@turf/bbox'
+import { Feature, featureCollection } from "@turf/helpers";
 // @ts-ignore
 import booleanOverlap from '@turf/boolean-overlap';
 // @ts-ignore
 import union from "@turf/union";
-// @ts-ignore
-import bboxPolygon from "@turf/bbox-polygon";
+
 
 export function long2tile(lon: number, zoom: number) {
   return (Math.floor((lon + 180) / 360 * Math.pow(2, zoom)));
@@ -24,12 +21,6 @@ export function tile2long(x: number, z: number) {
 export function tile2lat(y: number, z: number) {
   var n = Math.PI - 2 * Math.PI * y / Math.pow(2, z);
   return (180 / Math.PI * Math.atan(0.5 * (Math.exp(n) - Math.exp(-n))));
-}
-
-export function _getTileRect(x: number, y: number, zoom: number) {
-  const c1 = point([tile2long(x, zoom), tile2lat(y, zoom)]);
-  const c2 = point([tile2long(x + 1, zoom), tile2lat(y + 1, zoom)]);
-  return bboxPolygon(BBOX(featureCollection([c1, c2])));
 }
 
 export function joinIntersectingPolygons(polygons: Feature) {
